5476ceda376f277841cd9dfe2ba7c92f4af6674a
Francisco Monotya <franciscomontoyacasual@gmail.com>
2023-07-15 03:54:16 -0600

    Interfaz 1.0
    Interfaz minimamente viable

---
+++ src/main/java/com/beex_in_beel/beex_store/BeexStoreApplication.java
@@ -0,0 +1,13 @@
+package com.beex_in_beel.beex_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeexStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeexStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beex_in_beel/beex_store/BeexStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beex_in_beel.beex_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeexStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
